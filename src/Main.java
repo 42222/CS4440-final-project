ba8f5607abb2ed525186f59f05a1931a7ede7f15
42222 <50253762+42222@users.noreply.github.com>
2025-04-28 16:26:57 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.awt.image.BufferedImage;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java Parallel Image Processor started.");
-        // TODO: Initialize GUI or image selection
+
+        BufferedImage img = Utils.loadImage("images/sample.png"); 
+
+        if (img != null) {
+            System.out.println("Image loaded successfully!");
+
+            Utils.saveImage(img, "images/output.png");
+            System.out.println("Image saved successfully!");
+        } else {
+            System.out.println("Image loading failed.");
+        }
     }
 }
