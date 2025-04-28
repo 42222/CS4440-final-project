58109e323932ab8acc92c4b4d833fc5eeb819474
42222 <50253762+42222@users.noreply.github.com>
2025-04-28 16:25:57 -0700

    Update Utils.java

--- src/Utils.java
+++ src/Utils.java
@@ -1,12 +1,22 @@
 import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class Utils {
     public static BufferedImage loadImage(String path) {
-        // TODO: Load image from the file system
-        return null;
+        try {
+            return ImageIO.read(new File(path));
+        } catch (Exception e) {
+            System.out.println("Failed to load image: " + e.getMessage());
+            return null;
+        }
     }
 
     public static void saveImage(BufferedImage image, String path) {
-        // TODO: Save image to the file system
+        try {
+            ImageIO.write(image, "png", new File(path));
+        } catch (Exception e) {
+            System.out.println("Failed to save image: " + e.getMessage());
+        }
     }
 }
